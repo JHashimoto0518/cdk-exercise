5c3844205e20bbfb930d031367e664e04ea2407f
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-03-31 15:18:10 +0900

    add: create dynamodb table

--- typescript/dynamodb/lib/dynamodb-stack.ts
+++ typescript/dynamodb/lib/dynamodb-stack.ts
@@ -1,16 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
-export class DynamodbStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class DynamodbStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // simple table
+    const table = new Table(this, 'items', {
+      partitionKey: {
+        name: 'itemId',
+        type: AttributeType.STRING
+      },
+      tableName: 'items',
+      removalPolicy: RemovalPolicy.DESTROY, // default RemovalPolicy is RETAIN
+    });
   }
 }
