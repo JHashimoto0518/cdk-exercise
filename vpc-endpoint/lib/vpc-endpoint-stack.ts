246306f5f32d6bd124d15a4a46510239814e63d1
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-04-12 17:04:29 +0900

    add: vpc

--- vpc-endpoint/lib/vpc-endpoint-stack.ts
+++ vpc-endpoint/lib/vpc-endpoint-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-export class VpcEndpointStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class VpcEndpointStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcEndpointQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // vpc
+    const vpc = new ec2.Vpc(this, 'DbVpc', {
+      vpcName: 'db-vpc',
+      ipAddresses: ec2.IpAddresses.cidr('172.16.0.0/16'),
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        }
+      ],
+      // remove all rules from default security group
+      // See: https://docs.aws.amazon.com/config/latest/developerguide/vpc-default-security-group-closed.html
+      restrictDefaultSecurityGroup: true,
+    });
   }
 }
