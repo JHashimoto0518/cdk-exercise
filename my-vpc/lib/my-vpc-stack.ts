8a6092e7416d930c0ecdc757f7d202664d79b4e7
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-05-15 07:04:35 +0900

    fix: some errors

--- my-vpc/lib/my-vpc-stack.ts
+++ my-vpc/lib/my-vpc-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Vpc, SubnetType, InstanceType, InstanceClass, InstanceSize, MachineImage } from 'aws-cdk-lib/aws-ec2';
+import { Vpc, SubnetType, InstanceType, InstanceClass, InstanceSize, MachineImage, Instance, Port } from 'aws-cdk-lib/aws-ec2';
 import { ApplicationLoadBalancer, ApplicationProtocol, ListenerAction } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as elbv2_tg from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets'
 import { Construct } from 'constructs';
 
 export class MyVpcStack extends Stack {
@@ -17,7 +17,7 @@ export class MyVpcStack extends Stack {
           name: 'PublicSubnet',
         },
         {
-          subnetType: SubnetType.PRIVATE_WITH_NAT,
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
           name: 'PrivateSubnet',
         },
       ],
@@ -39,28 +39,30 @@ export class MyVpcStack extends Stack {
     });
 
     // Add EC2 instances in the private subnets
-    const ec2Instance1 = new ec2.Instance(this, 'EC2Instance1', {
+    const ec2Instance1 = new Instance(this, 'EC2Instance1', {
       vpc,
       instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
-      machineImage: MachineImage.latestAmazonLinux(),
+      machineImage: MachineImage.latestAmazonLinux2023(),
       vpcSubnets: {
-        subnetType: SubnetType.PRIVATE_WITH_NAT,
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
       },
     });
+    ec2Instance1.connections.allowFrom(alb, Port.tcp(80), 'Allow inbound traffic on port 80 from the ALB only');
 
-    const ec2Instance2 = new ec2.Instance(this, 'EC2Instance2', {
+    const ec2Instance2 = new Instance(this, 'EC2Instance2', {
       vpc,
       instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
-      machineImage: MachineImage.latestAmazonLinux(),
+      machineImage: MachineImage.latestAmazonLinux2023(),
       vpcSubnets: {
-        subnetType: SubnetType.PRIVATE_WITH_NAT,
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
       },
     });
+    ec2Instance2.connections.allowFrom(alb, Port.tcp(80), 'Allow inbound traffic on port 80 from the ALB only');
 
     // Add the instances to the ALB target group
     listener.addTargets('EC2Targets', {
       port: 80,
-      targets: [ec2Instance1, ec2Instance2],
+      targets: [new elbv2_tg.InstanceTarget(ec2Instance1), new elbv2_tg.InstanceTarget(ec2Instance2)],
     });
   }
 }
