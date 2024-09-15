21260501ce2a95d584de6e22b153f2adf1f70887
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-09-15 13:44:12 +0900

    add: cloudfront and origin bucket

--- cloudfront/lib/cloudfront-stack.ts
+++ cloudfront/lib/cloudfront-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput } from "aws-cdk-lib";
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class CloudfrontStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3 Bucket
+    const bucket = new s3.Bucket(this, 'HostingBucket', {
+      // bucketName: '20240913-angel-calendar',
+      enforceSSL: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudfrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudFront
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        // OAC
+        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
+      },
+    });
+
+    new CfnOutput(this, 'Distribution DomainName', { value: distribution.domainName })
   }
 }
