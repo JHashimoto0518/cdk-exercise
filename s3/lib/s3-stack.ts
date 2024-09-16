888665302491913e18d6f24931b06222b6e2f06c
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-09-16 15:30:42 +0900

    add: encryption bucket

--- s3/lib/s3-stack.ts
+++ s3/lib/s3-stack.ts
@@ -1,5 +1,6 @@
 import { RemovalPolicy, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as kms from 'aws-cdk-lib/aws-kms';
 import { Construct } from 'constructs';
 
 export class S3Stack extends Stack {
@@ -10,10 +11,33 @@ export class S3Stack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
+    new CfnOutput(this, 'SimpleBucketOutput', {
+      value: simpleBucket.bucketName,
+    });
+
+    // enable encryption for the bucket using a KMS key
+    const cmk = new kms.Key(this, 'BucketEncryptionKey', {
+      description: 'CMK for bucket encryption',
+      enableKeyRotation: false,
+    });
+    const encryptedBucket = new s3.Bucket(this, 'EncryptedBucket', {
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: cmk,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new CfnOutput(this, 'EncryptedBucketOutput', {
+      value: encryptedBucket.bucketName,
+    });
+
     // for web site hosting
     const webBucket = new s3.Bucket(this, 'WebBucket', {
       removalPolicy: RemovalPolicy.DESTROY,
       websiteIndexDocument: 'index.html',
+    });
+
+    new CfnOutput(this, 'WebBucketOutput', {
+      value: webBucket.bucketName,
     });
   }
 }
