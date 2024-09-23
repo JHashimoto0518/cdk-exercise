810a5cfc28efe66dc8a0ab8c98971a2f444b0b4e
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-09-23 18:41:53 +0900

    add: resources to iam stack

--- iam/lib/iam-stack.ts
+++ iam/lib/iam-stack.ts
@@ -7,8 +7,61 @@ export class IamStack extends cdk.Stack {
     super(scope, id, props);
 
     // group
+    const ec2Policy = new iam.Policy(this, 'Ec2Policy', {
+      document: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['ec2:Describe*'],
+            resources: ['*'],
+          }),
+        ],
+      }),
+    });
+
+    const rdsPolicy = new iam.Policy(this, 'RdsPolicy', {
+      document: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['rds:Describe*'],
+            resources: ['*'],
+          }),
+        ],
+      }),
+    });
+
     const group = new iam.Group(this, 'Group', {
       groupName: 'MyGroup',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBReadOnlyAccess'),
+      ],
     });
+    group.attachInlinePolicy(ec2Policy);
+    group.attachInlinePolicy(rdsPolicy);
+
+    // user
+    const user1 = new iam.User(this, 'User', {
+      userName: 'MyUser1',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBReadOnlyAccess'),
+      ],
+    });
+    user1.addToGroup(group);
+    user1.attachInlinePolicy(ec2Policy);
+    user1.attachInlinePolicy(rdsPolicy);
+
+    const user2 = new iam.User(this, 'User2', {
+      userName: 'MyUser2',
+    });
+    user2.addToGroup(group);
+
+    // role
+    const role = new iam.Role(this, 'Role', {
+      roleName: 'MyRole',
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+    role.attachInlinePolicy(ec2Policy);
+    role.attachInlinePolicy(rdsPolicy);
   }
 }
