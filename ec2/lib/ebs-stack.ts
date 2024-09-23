e03f57dc072f410cf62d258ff3a489db6607a89d
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-09-23 11:28:30 +0900

    add: create ec2 stack

---
+++ ec2/lib/ebs-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class EbsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // ebs volume
+    const volume = new ec2.Volume(this, 'Volume', {
+      availabilityZone: 'ap-northeast-1a',
+      size: cdk.Size.gibibytes(8),
+      encrypted: true,
+    });
+  }
+}
--- ec2/lib/ec2-stack.ts
+++ ec2/lib/ec2-stack.ts
@@ -6,11 +6,54 @@ export class Ec2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // ebs volume
-    const volume = new ec2.Volume(this, 'Volume', {
-      availabilityZone: 'ap-northeast-1a',
-      size: cdk.Size.gibibytes(8),
-      encrypted: true,
+    // vpc
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      vpcName: 'vpc',
+      ipAddresses: ec2.IpAddresses.cidr('172.16.0.0/16'),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        }
+      ],
+      // remove all rules from default security group
+      // See: https://docs.aws.amazon.com/config/latest/developerguide/vpc-default-security-group-closed.html
+      restrictDefaultSecurityGroup: true,
     });
+
+    // security group
+    const ec2Sg = new ec2.SecurityGroup(this, 'Ec2Sg', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'security group for ec2'
+    })
+    ec2Sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anywhere')
+
+    // ec2
+    const ec2Ins = new ec2.Instance(this, 'Ec2', {
+      instanceName: 'ec2',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }),
+      securityGroup: ec2Sg,
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(8, {
+            encrypted: true
+          }),
+        },
+      ],
+    })
   }
 }
